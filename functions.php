2f90eeaa7eeb29312ebf38369da101ce8b57bb03
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-28 21:55:56 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+//$link = mysqli_connect("localhost:3306", "root", "root", "mypreciousdb");
+$link = mysqli_connect("128.199.113.44:3306", "nida", "nidaproject1", "car_dealer");
+
+$page_name = "CDAM - Car Dealer Activity Management"
+ ?>
