499b9e3b79314e80942bef27ed5c0e05f1b666d0
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-28 21:56:03 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+	  
+	<link rel="icon" href="favicon.ico">
+    <meta charset="utf-8">
+    
+    <title><?php include("functions.php"); echo $page_name; ?></title>
+  </head>
+
+  <body>
+    <header>
+		<div id="headerwarp">
+	 		<h1 id="header_text"><?php echo $page_name; ?></h1>
+		</div>
+    </header>
