3a151fb40bf9dc151a82959104e1e1fe4a2cfdd8
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-28 21:55:52 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+
+<br><br>
+<hr>
+<div id="footer">
+    <p>Thanks for your supporting. Hope you are doing well. <br>
+    If you have problems or questions, Feel free to contact our support details as below.<br></p>
+    <p>Taninwat W. & Parinya P.<br>
+    Your friend<br>
+    MBA Faculty, NIDA<br></p>
+    <p>Mobile: (+66) 86-415-6156<br>
+    Email: taninwat.w@gmail.com<br>
+    </p>
+</div>
+<hr>
+
+</footer>
